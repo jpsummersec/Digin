65352f67060484f96940950bb063e36dadcb19e5
OlehHaliuk <oleh.haliuk@student.nhlstenden.com>
2026-05-13 10:04:52 +0200

    Recovered accidentally deleted config-example.php file

---
+++ test/php/config-example.php
@@ -0,0 +1,8 @@
+<!-- Replace with your real API key, and then rename this file to "config.php" for it to work with the "lorenzotest.php" file -->
+<?php
+
+return [
+    'api_key' => 'API_KEY_HERE',
+];
+
+?>
