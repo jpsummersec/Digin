2ef6bc29f4a42c0e17f09890ffcf6c788d60ccbf
Michael O. Boateng <michael.boateng1@student.nhlstenden.com>
2026-06-04 13:24:01 +0200

    landing.php  move from src folder to php folder

--- docker/app/public/php/landing.php
+++ docker/app/public/php/landing.php
@@ -23,6 +23,7 @@ $hero_buttons = [
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
   <title><?= htmlspecialchars($page_title) ?></title>
   <link rel="stylesheet" href="../css/landing.css" />
+  <link rel="stylesheet" href="../css/root.css">
 </head>
 
 <body>
