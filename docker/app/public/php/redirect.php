6926ddf56d4d3951eaa35edc2fb6436b99c5c05b
Lorenzo-Epic <lorenzo.nocivelli1@student.nhlstenden.com>
2026-06-08 18:40:23 +0200

    added bouncing animation and audio to the redirect page, but browser blocks autoplau

--- docker/app/public/php/redirect.php
+++ docker/app/public/php/redirect.php
@@ -9,7 +9,6 @@ include("include-dbhandler.php");
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="refresh" content="5;url=search-page.php">
     <title>REDIRECT</title>
     <link rel="stylesheet" href="../css/root.css">
     <link rel="stylesheet" href="../css/redirect.css">
@@ -19,14 +18,23 @@ include("include-dbhandler.php");
     <div id="redirect-container">
         <p id="thank-you">Thank you for</p>
         <img id="brand" src="../images/diggingin_logo.svg" alt="Digging In">
+        <audio id="thank-you-audio" src="../audio/thank-you-thank-you-thank-you-meme.mp3" preload="auto"></audio>
         <p id="redirect-message">You will be redirected<br>in <span id="countdown">5</span> seconds.</p>
         <p id="stuck">Stuck on this page? <a href="search-page.php">Click here</a></p>
     </div>
 
     <script>
         let seconds = 5;
+        const delayMs = 500;
 
         const countdown = document.getElementById('countdown');
+        const thankYouAudio = document.getElementById('thank-you-audio');
+
+        setTimeout(function() {
+            thankYouAudio.play().catch(function(error) {
+                console.log('Failed to play thank you audio:', error);
+            });
+        }, delayMs);
 
         const interval = setInterval(function() {
             seconds--;
