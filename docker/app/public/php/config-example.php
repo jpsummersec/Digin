4657752a67a1105ec37c545526b264f0996c83e5
Michael O. Boateng <michael.boateng1@student.nhlstenden.com>
2026-06-11 13:56:23 +0200

    pushing new changes

--- docker/app/public/php/config-example.php
+++ docker/app/public/php/config-example.php
@@ -1,9 +1,9 @@
 <?php
 
-require_once __DIR__ . '/include-url-config.php';
-
 return [
-    'api_key' => '',
+    'SPOTIFY_CLIENT_ID' => 'e80027eca4c74c98aa0707e8a143ba9c',
+    'SPOTIFY_CLIENT_SECRET' => 'e50886904b9e41179f742f942aced6ea',
+    'SPOTIFY_REDIRECT_URI' => 'http://127.0.0.1:3000/Spotify_connection/callback.php'
 ];
 
 ?>
